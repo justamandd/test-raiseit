828f40cae9698a0fad318daca651c98f1e93a382
Vitor Justamand <justamandd@gmail.com>
2024-10-25 16:17:40 -0300

    test: get pokemons by name controller added

---
+++ src/test/useCases/GetPokemonByName.test.ts/GetPokemonByNameController.test.ts
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import request from 'supertest';
+import express, { Express, Request, Response } from 'express';
+import { BmiCalculator } from "@entities/BmiCalculator";
+import { GetPokemonByNameService } from "@services/GetPokemonByName/GetPokemonByNameService";
+import { GetPokemonByNameUseCase } from "@useCases/GetPokemonByName/GetPokemonByNameUseCase";
+import { GetPokemonByNameController } from "@useCases/GetPokemonByName/GetPokemonByNameController";
+
+const app: Express = express();
+app.use(express.json());
+
+const bmiCalculator = new BmiCalculator();
+
+const getPokemonByNameService = new GetPokemonByNameService(
+  process.env.POKEAPI_URL!, 
+  process.env.POKEAPI_FIND_ROUTE!, 
+  bmiCalculator
+);
+
+const getPokemonByNameUseCase = new GetPokemonByNameUseCase(getPokemonByNameService);
+const getPokemonByNameController = new GetPokemonByNameController(getPokemonByNameUseCase);
+
+app.get('/pokemon/:name', async (req: Request, res: Response) => await getPokemonByNameController.handle(req, res));
+
+describe('GetPokemonByNameController', () => {
+  it('should return a pokemon with status 200', async () => {
+    const response = await request(app).get('/pokemon/pikachu');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('name');
+    expect(response.body).toHaveProperty('height');
+    expect(response.body).toHaveProperty('weight');
+    expect(response.body).toHaveProperty('bmi');
+  })
+
+  it('should return a 400 status when pokemon is not found', async () => {
+    const response = await request(app).get('/pokemon/invalid_pokemon');
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('message');
+  });
+})
