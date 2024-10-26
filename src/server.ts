a619abc8a14d1b30a86bb0ec0d2c536e3fae16f0
Vitor Justamand <justamandd@gmail.com>
2024-10-26 03:19:31 -0300

    feat(docs): Swagger UI documentation added

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,17 @@ dotenv.config();
 import express, { Express } from "express";
 import { routes } from "@routes/index";
 
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDocument = YAML.load('pokemons_api_doc.yaml');
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}/`));
